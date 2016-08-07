1aaafaa01b1dff2333ce3c0ae87a8b913e8f75e4
John <4585006@gmail.com>
2016-08-07 21:13:54 +0800

    fix bug

--- luci/luci-app-adbyby/luasrc/controller/adbyby.lua
+++ luci/luci-app-adbyby/luasrc/controller/adbyby.lua
@@ -1,25 +1,10 @@
---[[
-LuCI - Lua Configuration Interface - adbyby support
-
-Script by oldoldstone@gmail.com 
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id$
-]]--
-
+--Mr.Z<zenghuaguo@hotmail.com>
 module("luci.controller.adbyby", package.seeall)
 
-function index()	
+function index()
 	if not nixio.fs.access("/etc/config/adbyby") then
 		return
 	end
-	local page 
-	page = entry({"admin", "services", "adbyby"}, cbi("adbyby"), _("adbyby"), 50)
-	page.i18n = "adbyby"
-	page.dependent = true
+	
+	entry({"admin", "services", "adbyby"}, cbi("adbyby"), _("广告屏蔽大师"), 56).dependent = true
 end
