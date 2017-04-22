130a7cd791b5a5026a6453810632445d24d6381f
w01230 <4585006@gmail.com>
2017-04-22 18:33:34 +0800

    luci:remove version check, new luci can not work.

--- luci/luci-app-koolproxy/luasrc/model/cbi/koolproxy/global.lua
+++ luci/luci-app-koolproxy/luasrc/model/cbi/koolproxy/global.lua
@@ -3,43 +3,10 @@ openwrt-dist-luci: koolproxy
 ]]--
 
 local ds = require "luci.dispatcher"
-local ipkg = require("luci.model.ipkg")
 local uci = luci.model.uci.cursor()
 
-local pkg_name
-local min_version = "3.0.0"
 local koolproxy = "koolproxy"
 local m, s, o
-
-function is_installed(name)
-	return ipkg.installed(name)
-end
-
-function get_version()
-	local version = "1.0.0-1"
-	ipkg.list_installed("koolproxy*", function(n, v, d)
-		pkg_name = n
-		version = v
-	end)
-	return version
-end
-
-function compare_versions(ver1, comp, ver2)
-	if not ver1 or not (#ver1 > 0)
-	or not comp or not (#comp > 0)
-	or not ver2 or not (#ver2 > 0) then
-		return nil
-	end
-	return luci.sys.call("opkg compare-versions '%s' '%s' '%s'" %{ver1, comp, ver2}) == 1
-end
-
-if compare_versions(min_version, ">>", get_version()) then
-	local tip = 'koolproxy not found'
-	if pkg_name then
-		tip = 'Please update the packages: %s' %{pkg_name}
-	end
-	return Map(koolproxy, translate("koolproxy"), '<b style="color:red">%s</b>' %{tip})
-end
 
 m = Map(koolproxy, translate("koolproxy"), translate("A powerful advertisement blocker"))
 m.template = "koolproxy/index"
