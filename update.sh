31071a122553248022c7b117edf362103cfe8848
w01230 <4585006@gmail.com>
2017-11-24 22:44:58 +0800

    remove shortcut-fe.patch to patch

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm data/lazy.txt
-wget https://github.com/adbyby/xwhyc-rules/master/raw/lazy.txt -O data/lazy.txt
-
-rm data/video.txt
-wget https://github.com/adbyby/xwhyc-rules/master/raw/video.txt -O data/video.txt
