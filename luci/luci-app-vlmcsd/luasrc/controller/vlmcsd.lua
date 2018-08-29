7cbdc4cf63d54f10d4483930fa4817a0b2c0534b
w01230 <4585006@gmail.com>
2018-08-29 11:35:38 +0800

    update vlmcsd

--- luci/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ luci/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -1,13 +1,11 @@
---[[
-openwrt-dist-luci: ShadowSocks
-]]--
-
 module("luci.controller.vlmcsd", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/vlmcsd") then
 		return
 	end
-	
-	entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS服务器")).dependent = true
+	local page
+	page = entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS Server"), 100)
+	page.i18n = "vlmcsd"
+	page.dependent = true
 end
--- luci/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd.lua
+++ luci/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd.lua
@@ -1,61 +1,48 @@
-local NXFS = require "nixio.fs"
-local SYS  = require "luci.sys"
+local fs = require "nixio.fs"
 
-if SYS.call("pidof vlmcsd >/dev/null") == 0 then
-	Status = "免配置自动激活密钥管理服务 运行中"
+local m, s
+
+if luci.sys.call("pidof vlmcsd >/dev/null") == 0 then
+	status = translate("Vlmcsd is running.")
 else
-	Status = "免配置自动激活密钥管理服务 未运行"
+	status = translate("Vlmcsd is not running.")
 end
 
 m = Map("vlmcsd")
-m.title	= translate("KMS服务器")
-m.description = translate(Status)
+m.title = translate("KMS Server")
+m.description = translate("A KMS Serever Emulator to active your Windows or Office")
 
 s = m:section(TypedSection, "vlmcsd")
-s.anonymous=true
+s.addremove = false
+s.anonymous = true
+s.description = translate(string.format("%s<br /><br />", status))
 
---基本设置
-s:tab("basic", translate("基本设置"))
+s:tab("basic", translate("Basic Setting"))
+enable = s:taboption("basic", Flag, "enabled", translate("Enable"))
+enable.rmempty = false
 
-o = s:taboption("basic", Flag, "enable")
-o.title = translate("开启")
-o.rmempty = false
+autoactivate = s:taboption("basic", Flag, "autoactivate", translate("Auto activate"))
+autoactivate.rmempty = false
 
-o = s:taboption("basic", Value, "port")
-o.title = translate("本地端口")
-o.datatype = "port"
-o.default = 1688
-o.placeholder = 1688
-o.rmempty = false
-
-o = s:taboption("basic", Flag, "use_conf_file")
-o.title = translate("使用配置")
-o.rmempty = false
---配置
-s:tab("config", translate("配置"))
-
-local file = "/usr/share/vlmcsd/vlmcsd.ini"
-o = s:taboption("config", TextValue, "configfile")
-o.description = translate("开头数字符号（＃）或分号（;）的每一行被视为注释，去除分号（;）启用选项。")
-o.rows = 20
-o.wrap = "off"
-o.cfgvalue = function(self, section)
-	return NXFS.readfile(file) or ""
+s:tab("config", translate("Config File"))
+config = s:taboption("config", Value, "config", "", translate("开头的数字符号（＃）或分号（;）的每一行被视为注释；删除（;）启用指定选项。"))
+config.template = "cbi/tvalue"
+config.rows = 20
+config.wrap = "off"
+function config.cfgvalue(self, section)
+	return fs.readfile("/etc/vlmcsd.ini") or ""
 end
-o.write = function(self, section, value)
-	NXFS.writefile(file, value:gsub("\r\n", "\n"))
-end
-
-s:tab("log", translate("日志记录"))
-
-local file = "/var/log/vlmcsd.log"
-o = s:taboption("log", TextValue, "logfile")
-o.rows = 20
-o.wrap = "on"
-o.cfgvalue = function(self, section)
-	return NXFS.readfile(file) or ""
-end
-o.write = function(self, section, value)
+function config.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+	else
+		value = ""
+	end
+	fs.writefile("/tmp/vlmcsd.ini", value)
+	if (luci.sys.call("cmp -s /tmp/vlmcsd.ini /etc/vlmcsd.ini") == 1) then
+		fs.writefile("/etc/vlmcsd.ini", value)
+	end
+	fs.remove("/tmp/vlmcsd.ini")
 end
 
 return m
